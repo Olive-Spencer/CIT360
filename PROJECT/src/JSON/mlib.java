97e1a2ce9effd0800984338fc322bd96632bbb2b
Olive-Spencer <35235065+Olive-Spencer@users.noreply.github.com>
2019-11-02 20:57:13 -0600

    Json with object

--- PROJECT/src/JSON/mlib.java
+++ PROJECT/src/JSON/mlib.java
@@ -15,35 +15,62 @@ import org.json.simple.parser.ParseException;
  *
  * @author spencer
  */
+
+
 public class mlib {
     
      public static void main(String[] args) {
          
          
          
-         exp();
-         imp();
+         new mlib().exp();
+         new mlib().imp();
          
      }
+     
+    public class vars { 
+    
+    String name = "vars";
+    String business;
+    String activity;
+    String obj;
+    String name2;
+    
+}
+        
+
+
+
+
          
     
-    static void exp() {
+    void exp() {
         
         JSONObject obj = new JSONObject();
         Scanner in = new Scanner(System.in);
+        vars vars1 = new vars();
+        
         System.out.print("Enter a name \n");
         
         //creates a json object and adds items to it
         
-        obj.put("name",in.nextLine());
+        
+        vars1.name = in.nextLine();
         System.out.print("Enter a business\n");
-        obj.put("business",in.nextLine());
+        vars1.business = in.nextLine();
         System.out.print("Enter a activity \n");
-        obj.put("activity",in.nextLine());
+        vars1.activity = in.nextLine();
         System.out.print("Enter plural object \n");
-        obj.put("obj",in.nextLine());
+        vars1.obj = in.nextLine();
         System.out.print("Enter another name \n");
-        obj.put("name2",in.nextLine());
+        vars1.name2 = in.nextLine();
+        
+        
+        obj.put("name",vars1.name);
+        obj.put("business",vars1.business);
+        obj.put("activity",vars1.activity);
+        obj.put("obj",vars1.obj);
+        obj.put("name2",vars1.name2);
         
 
         //writes object to json file.
@@ -61,30 +88,31 @@ public class mlib {
         
          
        //import json file to java
-       static void imp(){
+       void imp(){
            //create a new parser
             JSONParser parser = new JSONParser();
         // try to import a file into a new reader
         try (Reader reader = new FileReader("k:\\projects\\madlib.json")) {
             
-            //create ajson object to parse the reader
+            //create a json object to parse the reader
             JSONObject jsonObject = (JSONObject) parser.parse(reader);
             //prints out the full jsonobject
             
+            vars vars2 = new vars();
             
             //prints out the jsonobject "name"
-            String name = (String) jsonObject.get("name");
-            String bus = (String) jsonObject.get("business");
-            String act = (String) jsonObject.get("activity");
-            String object = (String) jsonObject.get("obj");
-            String name2 = (String) jsonObject.get("name2");
+            vars2.name = (String) jsonObject.get("name");
+            vars2.business = (String) jsonObject.get("business");
+            vars2.activity = (String) jsonObject.get("activity");
+            vars2.obj = (String) jsonObject.get("obj");
+            vars2.name2 = (String) jsonObject.get("name2");
             
-            System.out.print("Dear Mr or Mrs. " + name + "\n" +
-                    "We regret to inform you that your application to " + bus + " has been denied.\n"
-                    +"Due to your lack of knowledge in " + act + " it won’t be possible \n"
+            System.out.print("Dear Mr or Mrs. " + vars2.name + "\n" +
+                    "We regret to inform you that your application to " + vars2.business + " has been denied.\n"
+                    +"Due to your lack of knowledge in " + vars2.activity + " it won’t be possible \n"
                     + "to continue your application process.\n\n" + 
-                    "Perhaps if you spend more time focusing on " + object + "\nwe may be able to consider you in the future. \n" +
-                    "Sincerely, " + name2 + "\n");
+                    "Perhaps if you spend more time focusing on " + vars2.obj + "\nwe may be able to consider you in the future. \n" +
+                    "Sincerely, " + vars2.name2 + "\n");
 
 
             //catches exceptions if there are any
